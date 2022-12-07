942d2419730c9bb73f0c64a6b4a0590b55e43da6
Winv <pro.dorianlopez@gmail.com>
2022-12-07 10:20:40 +0100

    chore

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ session_start();
 require 'vendor/autoload.php';
 
 use iutnc\mf\view\AbstractView;
+use \iutnc\tweeterapp\auth\TweeterAuthentification;
 
 $configfile = parse_ini_file('conf/config.ini');
 
@@ -20,14 +21,14 @@ try {
     $router = new \iutnc\mf\router\Router();
 
     $router->addRoute('home', 'list_tweets',      '\iutnc\tweeterapp\control\HomeController');
-    $router->addRoute('view', 'view_tweet',       '\iutnc\tweeterapp\control\TweetController', \iutnc\tweeterapp\auth\TweeterAuthentification::ACCESS_LEVEL_USER);
-    $router->addRoute('user', 'view_user_tweets', '\iutnc\tweeterapp\control\UserController', \iutnc\tweeterapp\auth\TweeterAuthentification::ACCESS_LEVEL_USER);
-    $router->addRoute('view_tweet', 'view_tweet', '\iutnc\tweeterapp\control\TweetController', \iutnc\tweeterapp\auth\TweeterAuthentification::ACCESS_LEVEL_USER);
-    $router->addRoute('post', 'post_tweet', '\iutnc\tweeterapp\control\PostController', \iutnc\tweeterapp\auth\TweeterAuthentification::ACCESS_LEVEL_USER);
+    $router->addRoute('view', 'view_tweet',       '\iutnc\tweeterapp\control\TweetController', TweeterAuthentification::ACCESS_LEVEL_USER);
+    $router->addRoute('user', 'view_user_tweets', '\iutnc\tweeterapp\control\UserController', TweeterAuthentification::ACCESS_LEVEL_USER);
+    $router->addRoute('view_tweet', 'view_tweet', '\iutnc\tweeterapp\control\TweetController', TweeterAuthentification::ACCESS_LEVEL_USER);
+    $router->addRoute('post', 'post_tweet', '\iutnc\tweeterapp\control\PostController', TweeterAuthentification::ACCESS_LEVEL_USER);
     $router->addRoute('signup','signup', '\iutnc\tweeterapp\control\SignUpController');
     $router->addRoute('login','login','\iutnc\tweeterapp\control\LoginController');
-    $router->addRoute('logout','logout','\iutnc\tweeterapp\control\LogoutController', \iutnc\tweeterapp\auth\TweeterAuthentification::ACCESS_LEVEL_USER);
-    $router->addRoute('following','view_following','\iutnc\tweeterapp\control\FollowingController', \iutnc\tweeterapp\auth\TweeterAuthentification::ACCESS_LEVEL_USER);
+    $router->addRoute('logout','logout','\iutnc\tweeterapp\control\LogoutController', TweeterAuthentification::ACCESS_LEVEL_USER);
+    $router->addRoute('following','view_following','\iutnc\tweeterapp\control\FollowingController', TweeterAuthentification::ACCESS_LEVEL_USER);
 
     $router->setDefaultRoute('list_tweets');
 
