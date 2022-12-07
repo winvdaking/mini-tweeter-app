cc71620dacbcf5a37e05f02bf96cfcfd46f0bc45
Winv <pro.dorianlopez@gmail.com>
2022-12-07 10:21:07 +0100

    chore TODO !

--- src/classes/tweeterapp/view/TweeterView.php
+++ src/classes/tweeterapp/view/TweeterView.php
@@ -22,25 +22,29 @@ abstract class TweeterView extends AbstractView implements Renderer
     public function makeBody(): string
     {
         $html = '';
-        $html .= $this->makeHeader();
+        $html .= $this->renderTopMenu();
         $html .= $this->render();
-        $html .= $this->makeFooter();
+
+        if(isset($_SESSION['user_profile']))
+            $html .= $this->renderBottomMenu();
+
+        $html .= $this->renderFooter();
 
         return  $html;
     }
 
-    public function makeHeader(): string
+    public function renderTopMenu(): string
     {        
         //A REFAIRE PROPREMENT
         $htmlHeaderCo = '<header class="theme-backcolor1"> 
         <h1>MiniTweeTR</h1>  
         <nav id="navbar">
         <a class="tweet-control" href="'. $this->router->urlFor('default') . '">
-        <img alt="home" ></a>
+        <img alt="home" src="https://homepages.loria.fr/ABoumaza/teaching/php-lp/project/figs/home.png"></a>
         <a class="tweet-control" href="'. $this->router->urlFor('login') .'">
-        <img alt="login" ></a>
+        <img alt="login" src="https://homepages.loria.fr/ABoumaza/teaching/php-lp/project/figs/login.png" ></a>
         <a class="tweet-control" href="'. $this->router->urlFor('signup') .'">
-        <img alt="signup"></a>
+        <img alt="signup" src="https://homepages.loria.fr/ABoumaza/teaching/php-lp/project/figs/signup.png"></a>
         </nav> 
         </header>';
 
@@ -48,24 +52,30 @@ abstract class TweeterView extends AbstractView implements Renderer
         <h1>MiniTweeTR</h1>  
         <nav id="navbar">
         <a class="tweet-control" href="'. $this->router->urlFor('default') . '">
-        <img alt="home" ></a>
-        <a class="tweet-control" href="'. $this->router->urlFor('home') .'">
-        <img alt="me" ></a>
+        <img alt="home" src="https://homepages.loria.fr/ABoumaza/teaching/php-lp/project/figs/home.png" ></a>
+        <a class="tweet-control" href="'. $this->router->urlFor('following') .'">
+        <img alt="following" src="https://homepages.loria.fr/ABoumaza/teaching/php-lp/project/figs/followees.png" ></a>
         <a class="tweet-control" href="'. $this->router->urlFor('logout') .'">
-        <img alt="logout"></a>
+        <img alt="logout" src="https://homepages.loria.fr/ABoumaza/teaching/php-lp/project/figs/logout.png"></a>
         </nav> 
         </header>';
 
         return !isset($_SESSION['user_profile']) ? $htmlHeaderCo : $htmlHeaderNotCo;
     }
 
-    public function makeFooter(): string
+    public function renderBottomMenu(): string
     {
         $htmlFooter = '';
         $htmlFooter .= '<nav id="menu" class="theme-backcolor1">
         <div id="nav-menu">
         <div class="button theme-backcolor2">
-        <a href="' . $this->router->urlFor('post', []) . '">New</a></div></div></nav>';
+        <a href="' . $this->router->urlFor('post', []) . '">Poster un tweet</a></div></div></nav>';
+        return $htmlFooter;
+    }
+
+    public function renderFooter(): string
+    {
+        $htmlFooter = '';
         $htmlFooter .= '<footer class="theme-backcolor1"> La super app créée en Licence Pro &copy;2018 Dorian</footer>';
         return $htmlFooter;
     }
--- src/classes/tweeterapp/view/UserView.php
+++ src/classes/tweeterapp/view/UserView.php
@@ -3,12 +3,14 @@
 namespace iutnc\tweeterapp\view;
 
 use iutnc\tweeterapp\model\User;
+use iutnc\tweeterapp\model\Follow;
 
 class UserView extends TweeterView{
 
     public function render(): string
     {
         $user = User::where('id', '=', $this->data[0]->author)->first();
+        $flw = Follow::where('followee', '=', $user->id)->where('follower', '=', $_SESSION['user_profile']['id'])->count();
 
         $html = '<article class="theme-backcolor2">';
         $html .= '<h2>Tweets from '. $user->username . ' ' . $user->fullname . '</h2><h3>'. $user->followers . ' follower</h3>';
@@ -28,6 +30,22 @@ class UserView extends TweeterView{
             $html .= "</div>";
             $html .= "</div>";
         }
+
+        if ($flw) {
+            $html .= '
+            <form class="forms" action="'. $this->router->urlFor('following') .'" method=post>
+            <input type="hidden" value="'. $user->id . '" name="followee">
+            <input type="hidden" value="true" name="unfollow">
+            <button type="submit">Ne plus suivre</button>
+            </form></article>';
+        }else{
+            $html .= '
+            <form class="forms" action="'. $this->router->urlFor('following') .'" method=post>
+            <input type="hidden" value="'. $user->id . '" name="followee">
+            <button type="submit">Suivre</button>
+            </form></article>';
+        }
+
         $html .= '</article>';
 
         return $html;
